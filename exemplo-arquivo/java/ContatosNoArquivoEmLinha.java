da5cd71f733a196cee7777e2db5d306c50e3c66a
Fábio Bezerra <fabio.bezerra@ufra.edu.br>
2023-09-18 06:22:18 -0300

    Como seria um banco de dados em arquivo

--- exemplo-arquivo/java/ContatosNoArquivoEmLinha.java
+++ exemplo-arquivo/java/ContatosNoArquivoEmLinha.java
@@ -1,8 +1,6 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.util.Scanner;
 
 public class ContatosNoArquivoEmLinha {
 
@@ -30,16 +28,93 @@ public class ContatosNoArquivoEmLinha {
         }
     }
 
-    public void inserirContato(String nome, String email, String telefone) {
-
-    }
-
     public void pesquisarPorNome(String nome) {
 
     }
 
+    public void inserirContato(String nome, String email, String telefone) {
+        try {
+            FileWriter fw = new FileWriter(this.ARQUIVO, true); //escreve no final do arquivo
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(nome);
+            bw.write(';');
+            bw.write(email);
+            bw.write(';');
+            bw.write(telefone);
+            bw.newLine();
+            bw.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void alterarContato(String nome, String email, String telefone) {
+
+    }
+
+    public void excluirContato(String nome) {
+
+    }
+
+    public void menu() {
+        System.out.println("Selecione uma operação com o banco de dados de contatos");
+        System.out.println();
+        System.out.println("1- listar contatos");
+        System.out.println("2- pesquisar contato");
+        System.out.println("3- incluir contato");
+        System.out.println("4- alterar contato");
+        System.out.println("5- excluir contato");
+        System.out.println();
+        System.out.print("> ");
+    }
+
     public static void main(String[] args) throws IOException {
         ContatosNoArquivoEmLinha contatos = new ContatosNoArquivoEmLinha();
-        contatos.listarContatos();
+        Scanner prompt = new Scanner(System.in);
+        String nome = null, email = null, telefone = null, opcao = null;
+        boolean encerrar = false;
+        while (!encerrar) {
+            contatos.menu();
+            opcao = prompt.nextLine();
+            switch (opcao.trim()) {
+                case "1":
+                    contatos.listarContatos();
+                    break;
+                case "2":
+                    System.out.print("Qual contato pesquisar?");
+                    nome = prompt.nextLine();
+                    contatos.pesquisarPorNome(nome);
+                    break;
+                case "3":
+                    System.out.println("Incluindo um contato no banco de dados ...");
+                    System.out.print("Qual o nome?");
+                    nome = prompt.nextLine();
+                    System.out.print("Qual o email?");
+                    email = prompt.nextLine();
+                    System.out.print("Qual o telefone?");
+                    telefone = prompt.nextLine();
+                    contatos.inserirContato(nome, email, telefone);
+                    break;
+                case "4":
+                    System.out.println("Incluindo um contato no banco de dados ...");
+                    System.out.print("Qual contato alterar?");
+                    nome = prompt.nextLine();
+                    System.out.print("Qual o novo email?");
+                    email = prompt.nextLine();
+                    System.out.print("Qual o novo telefone?");
+                    telefone = prompt.nextLine();
+                    contatos.alterarContato(nome, email, telefone);
+                    break;
+                case "5":
+                    System.out.println("Excluindo um contato no banco de dados ...");
+                    System.out.print("Qual contato excluir?");
+                    nome = prompt.nextLine();
+                    contatos.excluirContato(nome);
+                    break;
+                default:
+                    encerrar = true;
+            }
+        }
+        prompt.close();
     }
 }
