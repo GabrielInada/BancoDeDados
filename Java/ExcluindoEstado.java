ec3480b4d0c0b98506e33914cd1fe66a169d695c
Fábio Bezerra <fabio.bezerra@ufra.edu.br>
2024-12-06 17:05:22 -0300

    Uso do driver com.mysql.cj.jdbc.Driver nos exemplos das aulas

--- Java/ExcluindoEstado.java
+++ Java/ExcluindoEstado.java
@@ -7,7 +7,8 @@ public class ExcluindoEstado {
     public static void main(String[] args) {
         try {
             System.out.println("Conectando com o MySQL ...");
-            Class.forName("com.mysql.jdbc.Driver");
+//            Class.forName("com.mysql.jdbc.Driver"); //MySQL 5.X
+            Class.forName("com.mysql.cj.jdbc.Driver"); //MySQL 8.X
             String url = "jdbc:mysql://localhost:3306/aula_bd";
 
             Connection mydb = DriverManager.getConnection(url, "root", "root");
--- Java/InserindoEstado.java
+++ Java/InserindoEstado.java
@@ -7,14 +7,15 @@ public class InserindoEstado {
     public static void main(String[] args) {
         try {
             System.out.println("Conectando com o MySQL ...");
-            Class.forName("com.mysql.jdbc.Driver");
+//            Class.forName("com.mysql.jdbc.Driver"); //MySQL 5.X
+            Class.forName("com.mysql.cj.jdbc.Driver"); //MySQL 8.X
             String url = "jdbc:mysql://localhost:3306/aula_bd";
 
             Connection mydb = DriverManager.getConnection(url, "root", "root");
             
             String sql = "insert into estado (id, nome, sigla) values (?, ?, ?)";
             PreparedStatement ps = mydb.prepareStatement(sql);
-            ps.setInt(1, 6);
+            ps.setInt(1, 7);
             ps.setString(2, "Tocantins");
             ps.setString(3, "TO");
             int registros = ps.executeUpdate();
